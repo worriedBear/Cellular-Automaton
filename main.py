1d5e83e389699534158fa5527c8415c9b2c9f9c5
Friessnig <a.friessnig@student.tugraz.at>
2021-01-11 13:28:49 +0100

    add Rule 57, add simple input-mechanics

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from graphics import *
 
+global USER_INPUT
+
 
 # representing a single cell
 class Cell:
@@ -86,23 +88,33 @@ class Cell:
             self.right_cell.check_right_cell()
 
     def check_single_cell(self):
-        # a neighbor being "False" is the same is the neighbor being dead
-        if not self.left_cell or not self.left_cell.is_alive:
-            left = 0
-        else:
-            left = 1
-        if not self.right_cell or not self.right_cell.is_alive:
-            right = 0
-        else:
-            right = 1
-
         if self.is_alive:
             mid = 1
         else:
             mid = 0
-        self.evaluate_single_cell(left, mid, right)
+        # if this very cell is the leftmost cell - act as if the left neighbor
+        # was in the same state as the current cell - same applies for the
+        # rightmost cell
+        if not self.left_cell:
+            left = mid
+        elif self.left_cell.is_alive:
+            left = 1
+        else:
+            left = 0
 
-    def evaluate_single_cell(self, left, mid, right):
+        if not self.right_cell:
+            right = mid
+        elif self.right_cell.is_alive:
+            right = 1
+        else:
+            right = 0
+        # evaluate cells with respect to the current rule
+        if USER_INPUT == 30:
+            self.evaluate_rule_30(left, mid, right)
+        elif USER_INPUT == 57:
+            self.evaluate_rule_57(left, mid, right)
+
+    def evaluate_rule_30(self, left, mid, right):
         if left:
             if mid and right:
                 self.next_iter_alive = False
@@ -122,6 +134,26 @@ class Cell:
             elif not mid and not right:
                 self.next_iter_alive = False
 
+    def evaluate_rule_57(self, left, mid, right):
+        if left:
+            if mid and right:
+                self.next_iter_alive = False
+            elif mid and not right:
+                self.next_iter_alive = False
+            elif not mid and right:
+                self.next_iter_alive = True
+            elif not mid and not right:
+                self.next_iter_alive = True
+        else:
+            if mid and right:
+                self.next_iter_alive = True
+            elif mid and not right:
+                self.next_iter_alive = False
+            elif not mid and right:
+                self.next_iter_alive = False
+            elif not mid and not right:
+                self.next_iter_alive = True
+
     def update_all(self):
         self.is_alive = self.next_iter_alive
         if self.left_cell:
@@ -140,10 +172,25 @@ class Cell:
             self.right_cell.update_right_cell()
 
 
+def get_user_input_():
+    global USER_INPUT
+    valid_input = False
+    while not valid_input:
+        print("Enter the rule that will be applied by the cellular automaton\n"
+              "Currently, you can either enter >>30<< or >>57<< : ")
+        USER_INPUT = input()
+        if USER_INPUT.isnumeric() and \
+                (int(USER_INPUT) == 30 or int(USER_INPUT) == 57):
+            valid_input = True
+            USER_INPUT = int(USER_INPUT)
+        else:
+            print("INVALID INPUT....try again :/")
+
+
 def main():
     # set size of the appearing window
     width = 960
-    height = 500
+    height = 490
 
     # set number of iterations to be executed
     nr_iterations = 110
@@ -152,17 +199,20 @@ def main():
     starting_x = (width / 2) - 1
     starting_y = 20
 
+    # ask user which rule should be executed
+    get_user_input_()
+
     # create window
-    win = GraphWin("Rule 30", width, height)
+    win = GraphWin("Cellular Automaton", width, height)
     starting_point = Point(starting_x, starting_y)
     first_cell = Cell(False, False, True, starting_point.x, starting_point.y)
 
     for iteration in range(nr_iterations):
-        # add two new cells at each step (those new cells could only
-        # have been dead until now, so we can just construct them and
-        # set them to "dead" at each iteration)
+        # fill space with right number of dead cells
         first_cell.add_left_cell()
         first_cell.add_right_cell()
+
+    for iteration in range(nr_iterations):
 
         # output all current cells to window
         first_cell.output_all(win)
@@ -171,12 +221,11 @@ def main():
         # then update them to their new state
         first_cell.update_all()
 
-    # set coordinates of the rule-text
-    rule_x = width - 95
-    rule_y = 40
+    rule_x = width - 112
+    rule_y = 42
 
     # create text-object
-    rule_text = Text(Point(rule_x, rule_y), "Rule 30")
+    rule_text = Text(Point(rule_x, rule_y), "Rule " + str(USER_INPUT))
     rule_text.setSize(27)
     rule_text.setFace("times roman")
     rule_text.setStyle("italic")
